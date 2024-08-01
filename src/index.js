af89afbb57c23d3585238ebbdcce5fd470269dd7
Jenna <88835851+itcodegirl@users.noreply.github.com>
2024-08-01 16:43:51 -0500

    update index.js
    Display the current date here following this format "Today is Saturday, October 8, 2023"
    Display the date and time here 10 years from now follow this format "10 years from now, it will be Friday, October 8, 2033 11:43:32"

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import moment from "moment";
+//Display the current date here following this format "Today is Saturday, October 8, 2023"
 
 function displayCurrentDate() {
   let currentDate = moment().format("dddd, LL");
@@ -8,3 +8,26 @@ function displayCurrentDate() {
     todayCurrentDate.innerHTML = currentDate;
   }
 }
+
+//Update the time every second
+setInterval(displayCurrentDate, 1000);
+
+//Initial call to display the the time immediately on page load
+displayCurrentDate();
+
+//Display the date and time here 10 years from now follow this format "10
+//years from now, it will be Friday, October 8, 2033 11: 43: 32"
+
+function displayFutureDate() {
+  let futureDate = moment().add(10, 'years').format('dddd, MMMM D, YYYY HH:mm:ss');
+  console.log(futureDate);
+  let tenFutureDate = document.querySelector("#future-date");
+  console.log(tenFutureDate);
+  if (tenFutureDate) {
+    tenFutureDate.innerHTML = `10 years from now, it will be ${futureDate}`;
+  }
+
+}
+
+//Initial call to display the time immediately on page load
+displayFutureDate();
